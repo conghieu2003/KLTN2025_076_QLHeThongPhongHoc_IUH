3a66485bed54a1cfeaba4923870c4e1a5dcc47f9
conghieu2003 <letputsmile.onthatface0101@gmail.com>
2025-12-07 21:14:51 +0700

    update final

--- backend/src/routes/schedule.routes.js
+++ backend/src/routes/schedule.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const scheduleController = require('../controllers/schedule.controller');
 
-// Routes cho lịch học
 router.get('/schedules', scheduleController.getSchedules);
 router.get('/schedules/weekly', scheduleController.getWeeklySchedule);
 router.get('/schedules/print', scheduleController.printSchedule);
@@ -10,7 +9,6 @@ router.post('/schedules', scheduleController.createSchedule);
 router.put('/schedules/:id', scheduleController.updateSchedule);
 router.delete('/schedules/:id', scheduleController.deleteSchedule);
 
-// Routes cho dữ liệu filter
 router.get('/departments', scheduleController.getDepartments);
 router.get('/classes', scheduleController.getClasses);
 router.get('/teachers', scheduleController.getTeachers);
